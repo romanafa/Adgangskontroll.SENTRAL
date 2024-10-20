c976bcc630fb226da48e2641bd53884e07e8f279
Romana Faiglova <r.faiglova@gmail.com>
2024-10-20 17:10:29 +0200

    DB connection

---
+++ Adgangskontroll.SENTRAL/Data/DbConnection.cs
@@ -0,0 +1,26 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adgangskontroll.SENTRAL.Data
+{
+    public class DbConnection
+    {
+        private string _connectionString;
+
+        public DbConnection()
+        {
+            // ConnectionString ligger i App.config -> bruk config manager for å hente den
+            _connectionString = ConfigurationManager.ConnectionStrings["PostgresConnection"].ConnectionString;
+        }
+
+        public NpgsqlConnection GetConnection()
+        {
+            return new NpgsqlConnection(_connectionString);
+        }
+    }
+}
